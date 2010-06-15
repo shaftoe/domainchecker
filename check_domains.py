0f43cf49e61b5b7f3ac965e504a44581ed4fe0a7
Alexander Fortin <alexander.fortin@gmail.com>
2010-06-15 19:14:34 +0200

    Rifactored check_domains.py to add more error handling

--- check_domains.py
+++ check_domains.py
@@ -9,21 +9,33 @@ from Domain import Domain
 
 d = Domain()
 
-def scanDirectory(directory, fileextention=""):
+def scanDirectory(directory, fileextension=""):
     """Fetches files with given extentions from given directory
     Returns a domain list
     Prints out discovered files and statistics
     """
-    if not os.path.exists(directory):
-        raise OSError, "Directory %s doesn't exist" % directory
-    extentionlenght = len(fileextention) # check extention lenght cause it will be stripped from the filename to get real domain name
+    checkArgumentsValidity(directory,fileextension)
+    extentionlenght = len(fileextension) # check extention lenght cause it will be stripped from the filename to get real domain name
     for domainzonefilename in os.listdir(directory):
-        domainextention = domainzonefilename[-extentionlenght:]
-        counter = 0
-        if os.path.isfile(directory + "/" + domainzonefilename) and domainextention == fileextention:
-            domainname = domainzonefilename[:-extentionlenght]
-            print "\nFound domain zonefile %s" % domainzonefilename
+        domainextension = os.path.splitext(domainzonefilename)[1]
+        #counter = 0 TODO...
+        #check if files are not directories instead, and if they match fileextension too
+        if os.path.isfile(os.path.join(directory,domainzonefilename)) and domainextension == fileextension:
+            if extentionlenght != 0:
+                domainname = domainzonefilename[:-extentionlenght]
+            else:
+                domainname = domainzonefilename
+            print "\nFound domain zonefile %s\nProcessing %s..." % (domainzonefilename,domainname)
             if d.isExpired(domainname):
                 print "Domain %s is expired!" % domainname
             time.sleep(5)
     print "\nDone"
+
+def checkArgumentsValidity(directory,fileextension):
+    if not os.path.exists(directory):
+       raise OSError, "Directory %s doesn't exist" % directory
+    if len(fileextension) == 1:
+        raise TypeError, "Extension %s is not valid" % fileextension
+    if len(fileextension) != 0:
+        if fileextension[0:1] != "." :
+            raise TypeError, "Extension %s is not valid. Needs to start with a dot." % fileextension
