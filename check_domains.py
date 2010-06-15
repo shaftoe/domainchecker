f15a87771d391e5cfa2300de44e0dec7873413fd
Alexander Fortin <alexander.fortin@gmail.com>
2010-06-15 11:33:08 +0200

    New working version for the caller script

--- check_domains.py
+++ check_domains.py
@@ -7,20 +7,23 @@ import os.path
 import time
 from Domain import Domain
 
-def scanDirectory(directory, extension=""):
+d = Domain()
+
+def scanDirectory(directory, fileextention=""):
     """Fetches files with given extentions from given directory
     Returns a domain list
     Prints out discovered files and statistics
     """
     if not os.path.exists(directory):
         raise OSError, "Directory %s doesn't exist" % directory
-    extentionlenght = len(extension)
-    # os.path.abspath(directory)
-    domainextention = domainzonefiles[-extentionlenght:]
-    for domainzonefiles in os.listdir(directory):
-        if os.path.isfile(domainzonefiles) and domainextention == zonefileextention:
-            domainname = domainzonefiles[:-extentionlenght]
-            print "\nFound domain zonefile %s" % domainzonefiles
-            d.isExpired(domainname)
+    extentionlenght = len(fileextention) # check extention lenght cause it will be stripped from the filename to get real domain name
+    for domainzonefilename in os.listdir(directory):
+        domainextention = domainzonefilename[-extentionlenght:]
+        counter = 0
+        if os.path.isfile(directory + "/" + domainzonefilename) and domainextention == fileextention:
+            domainname = domainzonefilename[:-extentionlenght]
+            print "\nFound domain zonefile %s" % domainzonefilename
+            if d.isExpired(domainname):
+                print "Domain %s is expired!" % domainname
             time.sleep(5)
     print "\nDone"
